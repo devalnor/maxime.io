c6f8d05974e737f442639ef95b3fc28dadccba7e
Maxime de Visscher <maxime.de.visscher@gmail.com>
2020-02-04 17:06:22 +0100

    feat(content) Create Skills

---
+++ components/Skills.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { colors } from '../lib/theme';
+
+const Style = styled.section`
+  height: 4200px;
+`;
+
+const Skills = () => <Style>Empty</Style>;
+
+export default Skills;
