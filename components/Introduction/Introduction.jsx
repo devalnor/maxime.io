bc4539bbfc6bdca48d12f2128a8dbb37d7d82bca
Maxime de Visscher <maxime.de.visscher@gmail.com>
2020-02-06 16:26:17 +0100

    feat(content) Responsive Introduction

---
+++ components/Introduction/Introduction.jsx
@@ -0,0 +1,36 @@
+import { Media } from 'shared/utils/responsive';
+import {
+  Style, Container, Photo, Title, FullName, AuthorBox, Quote, About
+} from './Styles';
+
+
+const Introduction = () => (
+  <Style>
+    <Container>
+      <Media at="sm">
+        <Photo />
+      </Media>
+      <AuthorBox>
+        <FullName>Maxime de Visscher</FullName>
+        <Title>
+          <span>Technology Expert &amp; Digital Consultant</span>
+        </Title>
+      </AuthorBox>
+
+      <Quote>
+        Developing innovative solutions that bring Customer Experience into the business
+        goals <u>is a passion</u>.
+      </Quote>
+      <About>
+        I’m an experienced digital consultant with a broad skill set and deep
+        understanding of the interplay between <b>business</b>, <b>design</b>{' '}
+        and <b>technology</b>. Over the last decade, I helped customers crafting
+        solution that meet the holy grail of memorable user experiences in their
+        digital ecosystem.
+      </About>
+    </Container>
+  </Style>
+
+);
+
+export default Introduction;
---
+++ components/Introduction/Styles.js
@@ -0,0 +1,135 @@
+import styled from 'styled-components';
+import { colors } from 'shared/utils/theme';
+import { sizes, } from 'shared/utils/responsive';
+
+export const Style = styled.section`
+  background-color: ${colors.darkBackground};
+  color: white;
+  width: 100%;
+  height: 800px;
+  display: flex;
+  justify-content: center;
+  padding-left: 200px;
+  padding-right: 200px;
+
+  /* Desktop */
+  @media (min-width: ${sizes.md + 1}px) {
+  background-image: url('../static/img/me.png');
+  background-position-x: -430px;
+  background-repeat: no-repeat;
+  background-blend-mode: screen;
+  background-size: contain;
+  }
+
+  /* mobile */
+  @media (max-width: ${sizes.md}px) {
+  padding-left: 40px;
+  padding-right: 40px;
+  height: auto;
+  }
+`;
+
+export const Container = styled.div`
+  max-width: 1200px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  /* mobile */
+  @media (max-width: ${sizes.md}px) {
+    align-items: center;
+  }
+`;
+
+export const Quote = styled.div`
+  font-family: 'Montreal Regular';
+  max-width: 760px;
+  font-size: 45px;
+  letter-spacing: -0.031em;
+  margin-top: 30px;
+  margin-bottom: 50px;
+  u {
+    font-family: inherit;
+  }
+  /* Tablet */
+  @media (max-width: ${sizes.lg}px) {
+    font-size: 40px;
+    text-align: center
+  }
+
+  /* Mobile */
+  @media (max-width: ${sizes.md}px) {
+    font-size: 27px;
+    text-align: center
+
+  }
+`;
+
+export const About = styled.div`
+  letter-spacing: -0.022em;
+  max-width: 760px;
+  font-size: 21px;
+  font-weight: 200;
+  line-height: 40px;
+  b {
+    font-weight: 500;
+  }
+  padding-bottom:40px;
+  /* Mobile */
+  @media (max-width: ${sizes.md}px) {
+    font-size: 15px;
+    line-height: 27px;
+  }
+`;
+
+
+export const AuthorBox = styled.div`
+  max-width: 1200px;
+  width: 50%;
+  display: flex;
+  flex-direction: row;
+  
+  /* mobile */
+  @media (max-width: ${sizes.md}px) {
+    width: 200px;
+    flex-direction: column;
+  }
+`;
+
+export const FullName = styled.div`
+  background-color: ${colors.blueFlash};
+  padding: 10px;
+  color: white;
+  white-space: nowrap;
+  text-align:center;
+ 
+  /* mobile */
+  @media (max-width: ${sizes.md}px) {
+    padding: 5px;
+    font-weight: 500;
+  }
+`;
+
+export const Title = styled.div`
+  display: flex;
+  text-align: center;
+  color: white;
+  padding: 10px;
+  @media (min-width: ${sizes.md}px) {
+    white-space: nowrap;
+  }
+`;
+
+export const Photo = styled.div`
+  background-color: ${colors.darkBackground};
+  background-image: url('../static/img/me.png');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-blend-mode: screen;
+  background-size: contain;
+  display:block;
+  width: 100%;
+  height: 100%;
+  min-height:350px;
+  min-width:350px;
+`;
