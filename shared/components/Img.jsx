2ef6ddd8b897ab1aac8639eebf048113f3b76b93
Maxime de Visscher <maxime.de.visscher@gmail.com>
2020-02-08 16:01:05 +0100

    chore(shared) Improved Img components

--- shared/components/Img.jsx
+++ shared/components/Img.jsx
@@ -4,22 +4,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Img = ({
-  definitions, src, ...restprops
+  definitions, src, alt, title, ...restprops
 }) => {
-  const srcSet = definitions.map((def) => `${src.split('.').slice(0, -1).join('.')}@${def}.${src.split('.').pop()} ${def}`);
+  const srcSet = definitions.map(
+    (def) => `${src
+      .split('.')
+      .slice(0, -1)
+      .join('.')}@${def}.${src.split('.').pop()} ${def}`
+  );
   return (
-    <div>
-      <img src={src} srcSet={srcSet} {...restprops} />
-    </div>
+
+    <img src={src} srcSet={srcSet} {...restprops} alt={alt} title={title} />
+
   );
 };
 
-
 Img.propTypes = {
-  definitions: PropTypes.arrayOf(PropTypes.string).isRequired,
+  definitions: PropTypes.arrayOf(PropTypes.string),
   src: PropTypes.string.isRequired,
-  alt: PropTypes.string.isRequired
+  alt: PropTypes.string.isRequired,
+  title: PropTypes.string
 };
 
+Img.defaultProps = {
+  definitions: ['2x'],
+  title: ''
+};
 
 export default Img;
