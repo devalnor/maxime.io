8ca2cb2e7040dc717360d984cd0dc7eaca071544
Maxime de Visscher <maxime.de.visscher@gmail.com>
2020-02-08 16:02:02 +0100

    chore(content) Add titltes to technologies and remove optional definitions

--- components/Technologies/Technologies.jsx
+++ components/Technologies/Technologies.jsx
@@ -10,20 +10,20 @@ const Technologies = () => (
         <Box width="50%">
           <SmallTitle>Development Stack</SmallTitle>
           <LogoList>
-            <Img src="/static/img/logos/js.png" definitions={['2x']} alt="Javascript" />
-            <Img src="/static/img/logos/ts.png" definitions={['2x']} alt="Typescript" />
-            <Img src="/static/img/logos/react.png" definitions={['2x']} alt="React" />
-            <Img src="/static/img/logos/graphql.png" definitions={['2x']} alt="GraphQL" />
-            <Img src="/static/img/logos/postgre.png" definitions={['2x']} alt="PostgreSQL" />
-            <Img src="/static/img/logos/docker.png" definitions={['2x']} alt="Docker" />
+            <Img src="/static/img/logos/js.png" alt="Javascript" title="Javascript" />
+            <Img src="/static/img/logos/ts.png" alt="Typescript" title="Typescript" />
+            <Img src="/static/img/logos/react.png" alt="React" title="React" />
+            <Img src="/static/img/logos/graphql.png" alt="GraphQL" title="GraphQL" />
+            <Img src="/static/img/logos/postgre.png" alt="PostgreSQL" title="PostgreSQL" />
+            <Img src="/static/img/logos/docker.png" alt="Docker" title="Docker" />
           </LogoList>
         </Box>
         <Box width="50%">
           <SmallTitle>Cloud Services</SmallTitle>
           <LogoList>
-            <Img src="/static/img/logos/aws.png" definitions={['2x']} alt="Amazon Web Service" />
-            <Img src="/static/img/logos/heroku.png" definitions={['2x']} alt="Heroku" />
-            <Img src="/static/img/logos/azure.png" definitions={['2x']} alt="Microsoft Azure" />
+            <Img src="/static/img/logos/aws.png" alt="Amazon Web Service" title="Amazon Web Service" />
+            <Img src="/static/img/logos/heroku.png" alt="Heroku" title="Heroku" />
+            <Img src="/static/img/logos/azure.png" alt="Microsoft Azure" title="Microsoft Azure" />
           </LogoList>
 
         </Box>
@@ -32,17 +32,17 @@ const Technologies = () => (
         <Box width="50%">
           <SmallTitle>Prototyping</SmallTitle>
           <LogoList>
-            <Img src="/static/img/logos/xd.png" definitions={['2x']} alt="Adobe XD" />
-            <Img src="/static/img/logos/invision.png" definitions={['2x']} alt="Invision" />
-            <Img src="/static/img/logos/sketch.png" definitions={['2x']} alt="Sketch" />
+            <Img src="/static/img/logos/xd.png" alt="Adobe XD" title="Adobe XD" />
+            <Img src="/static/img/logos/invision.png" alt="Invision" title="Invision" />
+            <Img src="/static/img/logos/sketch.png" alt="Sketch" title="Sketch" />
           </LogoList>
         </Box>
         <Box width="50%">
           <SmallTitle>CI / CR &amps; Project Management</SmallTitle>
           <LogoList>
-            <Img src="/static/img/logos/github.png" definitions={['2x']} alt="Github" />
-            <Img src="/static/img/logos/gitlab.png" definitions={['2x']} alt="Gitlan" />
-            <Img src="/static/img/logos/jira.png" definitions={['2x']} alt="Atlasian JIRA" />
+            <Img src="/static/img/logos/github.png" alt="Github" title="Github" />
+            <Img src="/static/img/logos/gitlab.png" alt="Gitlab" title="Gitlab" />
+            <Img src="/static/img/logos/jira.png" alt="Atlasian JIRA" title="Atlasian JIRA" />
           </LogoList>
         </Box>
       </Row>
