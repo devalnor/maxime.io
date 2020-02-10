99575f2b7f3678b55759ee4b3d867a908ac173fb
Maxime de Visscher <maxime.de.visscher@gmail.com>
2020-02-10 15:50:30 +0100

    feat(content) WhatsApp preview

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -33,11 +33,28 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>
+            Maxime de Visscher - Technology Expert &amp; Digital Consultant
+          </title>
+          <meta
+            name="description"
+            c
+            ontent="I’m an experienced digital consultant with a broad skill set and deep understanding of the interplay between business, design and technology. Over the last decade, I helped customers crafting solution that meet the holy grail of memorable user experiences in their digital ecosystem."
+          />
+          <meta
+            property="og:title"
+            content="Maxime de Visscher - Technology Expert &amp; Digital Consultant"
+          />
+          <meta
+            property="og:description"
+            content="Developing innovative solutions that bring Customer Experience to your business goals is a passion."
+          />
           <style
             type="text/css"
             // eslint-disable-next-line react/no-danger
             dangerouslySetInnerHTML={{ __html: mediaStyles }}
           />
+          <meta property="og:image" content="/static/img/me.jpg" />
         </Head>
         <body>
           <Main />
