470af7ed65f10c3191675298729622ee02716931
Maxime de Visscher <maxime.de.visscher@gmail.com>
2020-02-11 15:51:12 +0100

    chore(header) Code cleaning

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -51,7 +51,10 @@ export default class MyDocument extends Document {
             // eslint-disable-next-line react/no-danger
             dangerouslySetInnerHTML={{ __html: mediaStyles }}
           />
-          <meta property="og:image" content="https://maxime.io/static/img/me.jpg" />
+          <meta
+            property="og:image"
+            content="https://maxime.io/static/img/me.jpg"
+          />
         </Head>
         <body>
           <Main />
