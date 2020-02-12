5fc7684733f1600d5038f88b75a03c83f669689c
Maxime de Visscher <maxime.de.visscher@gmail.com>
2020-02-12 01:05:55 +0100

    feat(app) Add Minimal LoadScreen component

---
+++ components/LoadScreen/Styles.js
@@ -0,0 +1,26 @@
+/* eslint-disable import/prefer-default-export */
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+export const Container = styled(motion.div)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: 999999;
+  color: white;
+  background-color: black;
+`;
+
+// Motion Framer Default Variants
+Container.defaultProps = {
+  transition: { duration: 2.5 },
+  innitial: 'show',
+  variants: {
+    showed: {
+      opacity: 1
+    },
+    hidden: { opacity: 0 }
+  }
+};
