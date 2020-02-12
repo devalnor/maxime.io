1b8715de1c349a4aec9488f728d64da36b3ff570
Maxime de Visscher <maxime.de.visscher@gmail.com>
2020-02-12 01:04:38 +0100

    feat(utils) Add custom FontObserver utils

---
+++ shared/utils/FontObserver.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import FontFaceObserver from 'fontfaceobserver';
+
+export const useFontFace = (fontFamilies) => {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  useEffect(() => {
+    const observers = fontFamilies.map((fontFamily) => new FontFaceObserver(fontFamily).load());
+
+    Promise.all(observers)
+      .then(() => {
+        setFontLoaded(true);
+      })
+      .catch(() => {
+        setFontLoaded(false);
+      });
+  }, [fontFamilies]);
+
+  return fontLoaded;
+};
+
+export const FontObserver = ({ fontFamily, children }) => {
+  const fontLoaded = useFontFace(fontFamily);
+
+  return <>{React.cloneElement(children, { fontLoaded })}</>;
+};
+
+FontObserver.propTypes = {
+  fontFamily: PropTypes.arrayOf(PropTypes.string).isRequired,
+  children: PropTypes.string.isRequired
+};
