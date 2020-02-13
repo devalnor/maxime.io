804e8171db9fb99f2eded4cc7f50f662002ab464
Maxime de Visscher <maxime.de.visscher@gmail.com>
2020-02-13 15:44:23 +0100

    chore(project) Secret commit copy

--- shared/utils/easterEgg.js
+++ shared/utils/easterEgg.js
@@ -3,7 +3,7 @@ const easterEgg = () => {
   if (typeof window !== 'undefined') {
     // eslint-disable-next-line no-console
     console.log(
-      '%c Hello hacker friend                        \n If you want to review the code, be my guest\nhttps://github.com/devalnor/maxime.io/',
+      '%c Hello hacker friend ;)                     \n If you want to review the code, be my guest\nhttps://github.com/devalnor/maxime.io/',
       'font-size:18px; background-color:#3865FF; color:white'
     );
   }
