1d467f82410c1870b482b4e531552b6ff3a581df
Maxime de Visscher <maxime.de.visscher@gmail.com>
2021-04-07 16:28:08 +0200

    fix(app) Missing React import

--- shared/utils/FontObserver.jsx
+++ shared/utils/FontObserver.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+
 import PropTypes from 'prop-types';
 import FontFaceObserver from 'fontfaceobserver';
 
